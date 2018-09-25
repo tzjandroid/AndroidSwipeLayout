3a5277ae2153f20958859a0ddaee1b833b9b01b1
tangzejin921 <tangzejin921@qq.com>
2018-09-25 15:13:46 +0800

    不用每次 findViewById

--- library/src/main/java/com/daimajia/swipe/implments/SwipeItemMangerImpl2.java
+++ library/src/main/java/com/daimajia/swipe/implments/SwipeItemMangerImpl2.java
@@ -2,6 +2,7 @@ package com.daimajia.swipe.implments;
 
 import android.view.View;
 
+import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.interfaces.SwipeAdapterInterface;
 
 /**
@@ -13,12 +14,36 @@ public class SwipeItemMangerImpl2 extends SwipeItemMangerImpl{
         super(swipeAdapterInterface);
     }
 
+    public void onCreate(View view,int position){
+        int resId = swipeAdapterInterface.getSwipeLayoutResourceId(position);
+        if (resId<=0){
+            return;
+        }
+        view.setTag(resId,view.findViewById(resId));
+    }
     @Override
     public void bind(View view, int position) {
         int resId = swipeAdapterInterface.getSwipeLayoutResourceId(position);
         if (resId<=0){
             return;
         }
-        super.bind(view, position);
+        int tagId = resId+1;//防止 view 为 SwipeLayout 覆盖掉
+        SwipeLayout swipeLayout = (SwipeLayout) view.getTag(resId);
+        if (swipeLayout == null)
+            throw new IllegalStateException("can not find SwipeLayout in target view");
+
+        if (swipeLayout.getTag(tagId) == null) {
+            OnLayoutListener onLayoutListener = new OnLayoutListener(position);
+            SwipeMemory swipeMemory = new SwipeMemory(position);
+            swipeLayout.addSwipeListener(swipeMemory);
+            swipeLayout.addOnLayoutListener(onLayoutListener);
+            swipeLayout.setTag(tagId, new ValueBox(position, swipeMemory, onLayoutListener));
+            mShownLayouts.add(swipeLayout);
+        } else {
+            ValueBox valueBox = (ValueBox) swipeLayout.getTag(tagId);
+            valueBox.swipeMemory.setPosition(position);
+            valueBox.onLayoutListener.setPosition(position);
+            valueBox.position = position;
+        }
     }
 }
